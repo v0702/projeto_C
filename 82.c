f083c7c0ac722fe1beb6a86caa4eda14ddc2905a
Victor <45666816+v0702@users.noreply.github.com>
2018-12-09 17:57:16 +0000

    Add files via upload

--- 82.c
+++ 82.c
@@ -18,48 +18,82 @@ int comesFirst(ESTUDANTE*, ESTUDANTE*); //checks name order
 void main()
 {
     ESTUDANTE num_A, num_B;
-    boolean X=0, Y;
+    boolean X, Y=0;
 
     writeStruct(&num_A);
-    while(X!=1)//while A==B then the program will keep asking you to
-    {          //rewrite B until they are different
+    do
+    {
         writeStruct(&num_B);
         X = equalEntry(&num_A, &num_B);//returns 1 if the they are equal
-        if(X!=1)//prints to the screen if the register are equal
-            printf("\nRegister already exist please try again.\n");
+        if(X==1)//prints to the screen if the register are equal
+            printf("Register already exist please try again.\n");
     }
+    while(X==1);//while A==B then the program will keep asking you to rewrite B until they are different
+
     Y = comesFirst(&num_A, &num_B);//returns 1 if num_A comes fist to num_B
 
     if(Y==1)
-        printf("\n%s %s\n comes first to\n%s %s\n", num_A.nome, num_A.apelido, num_B.nome, num_B.apelido);
+        printf("\n%s %s\ncomes first to\n%s %s\n", num_A.nome, num_A.apelido, num_B.nome, num_B.apelido);
     else
-         printf("\n%s %s\n comes first to\n%s %s\n", num_B.nome, num_B.apelido, num_A.nome, num_A.apelido);
+         printf("\n%s %s\ncomes first to\n%s %s\n", num_B.nome, num_B.apelido, num_A.nome, num_A.apelido);
 }
 
 void writeStruct(ESTUDANTE *nome)//writes the info on the student
 {
-    printf("\nType the name:");
+    printf("New entry:\n");
+    printf("Type the name:");
     scanf("%s", nome->nome);//using 'scanf' rather than 'gets' because I only need to one word, rather than a a phrase
-    printf("\nType the surname:");
+    printf("Type the surname:");
     scanf("%s", nome->apelido);
-    printf("\nType where you living:");
-    gets(nome->residencia);
-    printf("\nType your phone number:");
-    scanf("%d", nome->telefone);
+    printf("Type where you living:");
+    scanf("%s", nome->residencia);
+    printf("Type your phone number:");
+    scanf("%d", &nome->telefone);
 }
-int equalEntry (ESTUDANTE *A,ESTUDANTE *B)//check if they are equal
-{//TODO
+int equalEntry (ESTUDANTE *A, ESTUDANTE *B)//check if they are equal
+{
+    int i;
+    for(i=0;(A->nome[i] != '\0')||(B->nome[i] != '\0');++i)//keeps looping on the longest name
+    {
+        if(A->nome[i] != B->nome[i])//checks if the name is equal
+        {
+            return 0;//if this happens then the names are different thus return FALSE
+        }
+    }//if this finish then the name is the same
+    for(i=0;(A->apelido[i] != '\0')||(B->apelido[i] != '\0');++i)//keeps looping on the longest surname
+    {
+        if(A->apelido[i] != B->apelido[i])//checks if the surname is equal
+        {
+            return 0;//surnames are different
+        }
+    }//if this finish then the surname is the same
+    for(i=0;(A->residencia[i] != '\0')||(B->residencia[i] != '\0');++i)
+    {
+        if(A->residencia[i] != B->residencia[i])//checks if the home name is the same
+        {
+            return 0;//home is different
+        }
+    }//if this finish then the home is the same
+    if(A->telefone != B->telefone)//checks if the phone number is the same
+    {
+        return 0;//phone number is different
+    }//if this finish the the phone number is the same
+
+    //if all of this finish then the DATA is already existing
+    return 1;
+}
+int comesFirst(ESTUDANTE *A, ESTUDANTE *B)//checks what name comes first
+{
     maisc first_maisc, second_maisc;//only to be used if it is the first letter
     minsc first_minsc, second_minsc;//to be use the other times
-    int i=0;//will server to count syllables
-
-    while(A->nome[i] == B->nome[i])//if the syllables match it moves onto the next one;
+    first_maisc=A->nome[0];
+    second_maisc=B->nome[0];
+    if(first_maisc<second_maisc)
+        return 1;
+    else if(first_maisc>second_maisc)
+        return 0;
+    else//that being the first to letters are equal
     {
-        ++i;
-    }//if this ends it means the syllables don't match on the i
-
-}
-int comesFirst ()//TODO
-{
-    
+        //TODO
+    }
 }
